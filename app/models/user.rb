3dfc891188017cd85a26001b20d02f33d1549710
Emma Claire Philippides <acme9898@gmail.com>
2017-04-11 21:45:44 -0700

    added vote and user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :votes
+  has_many :listed_works, through: :votes, source: :work
+  validates :username, uniqueness: true, presence: true
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,2 +1,5 @@
 class Vote < ApplicationRecord
+  belongs_to :user
+  belongs_to :work #what more can I add here? maybe refer to counter?
+  validates :user #scope
 end
